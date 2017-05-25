f7da2a8f8ee5d71d4d85b8f5d71c7cba5a8ad727
rektide de la faye <rektide@voodoowarez.com>
2017-05-24 20:06:17 -0400

    Get turtle namespaces building.

--- ldspace.js
+++ ldspace.js
@@ -3,7 +3,7 @@ import { pimSpace, ldp } from "./ns"
 const _types= Object.freeze([ ldp.BasicContainer, ldp.Resource, pim.Workspace])
 
 class LdSpace{
-	get '@type'{
+	get '@type'() {
 		return _types
 	}
 }
---
+++ ns/index.js
@@ -0,0 +1,7 @@
+import ldp from "./ns-ldp"
+import pimSpace from "./ns-pim-space"
+
+export default {
+	ldp,
+	pimSpace
+}
